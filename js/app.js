5345eb06ab080cb575ccadc512bc5e2669bbc855
Viet Cuong Nguyen <nguyenvietcuong1999@gmail.com>
2021-08-22 23:57:22 +0700

    js part 1

--- js/app.js
+++ js/app.js
@@ -15,4 +15,62 @@ class UI {
     this.itemList = [];
     this.itemID = 0;
   }
+  //submit budget method
+  submitBudgetForm() {
+    const __this = this;
+    const value = this.budgetInput.value;
+
+    if (value==='' || value < 0) {
+      __this.budgetFeedback.classList.add('showItem');
+      __this.budgetFeedback.innerHTML = '<p>Value cannot be empty or negative</p>';
+      setTimeout(function() {
+        __this.budgetFeedback.classList.remove('showItem');
+      }, 4000);
+    } else {
+      __this.budgetAmount.textContent = value;
+      __this.budgetInput.value = '';
+      __this.showBalance();
+    }
+  }
+  
+  // Show balance
+  showBalance() {
+    const expense = this.totalExpense();
+    const total = parseInt()
+  }
+  // Total expense
+  totalExpense() {
+    let total = 400;
+    return total;
+  }
 }
+
+function eventListeners(){
+  const budgetForm = document.getElementById("budget-form");
+  const expenseForm = document.getElementById("expense-form");
+  const expenseList = document.getElementById("expense-list");
+
+  // Neew instance of UI class
+  const ui =  new UI();
+
+  // Budget form submit button
+  budgetForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    ui.submitBudgetForm();
+  })
+
+// Expense form submit button
+  expenseForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+  })
+
+  // Expense click
+  expenseList.addEventListener("click", function() {
+
+  })
+
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  eventListeners();
+})
